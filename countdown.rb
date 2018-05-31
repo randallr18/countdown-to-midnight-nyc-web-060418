56ac38bf43448a7c54c5854797cd93ba639a7760
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 04:48:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(seconds)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  number = seconds
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
   sleep 1
 end
 "HAPPY NEW YEAR!"
